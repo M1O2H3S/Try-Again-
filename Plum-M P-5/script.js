6a343ec9b5ba1b56a23a81f8a32a661a8f0bc5a3
Cellizon 111 <mohsinnadeem142@gmail.com>
2024-11-01 19:15:45 +0500

    update

---
+++ Plum-M P-5/script.js
@@ -0,0 +1,37 @@
+// Get the button
+  let backToTopBtn = document.getElementById("backToTopBtn");
+  
+  // When the user scrolls down 200px from the top of the document, show the button
+  window.onscroll = function() { scrollFunction() };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 3100 || document.documentElement.scrollTop > 3100) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  }
+  
+
+  function showSidebar() {
+    const sidebar = document.querySelector('.sidebar ')
+    sidebar.style.display = 'flex'
+  }
+  
+  function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar ')
+    sidebar.style.display = 'none'
+  }
+  
+  function is(src) {
+    window.location = src;
+  }
+  // When the user clicks on the button, scroll to the top of the document
+function opFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
---
+++ Price Pakages:-/$600-6/script.js
@@ -0,0 +1,87 @@
+// Get the button
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 3100 || document.documentElement.scrollTop > 3100) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function opFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+
+const sidebar = document.querySelector(".sidebar");
+const cross = document.querySelector(".fa-xmark");
+const black = document.querySelector(".black");
+const sidebtn = document.querySelector(".second-1");
+
+
+
+
+cross.addEventListener("click", () => {
+  sidebar.classlist.fa - xmark("active");
+  cross.classlist.remove("active");
+  black.classlist.remove("active");
+});
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar ')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar ')
+  sidebar.style.display = 'none'
+}
+
+function is(src) {
+  window.location = src;
+}
+
+/**Registration**/
+const form = document.querySelector("form"),
+  nextBtn = form.querySelector(".nextBtn"),
+  backBtn = form.querySelector(".backBtn"),
+  allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", () => {
+  allInput.forEach(input => {
+    if (input.value != "") {
+      form.classList.add('secActive');
+    } else {
+      form.classList.remove('secActive');
+    }
+  })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+
+function passaval() {
+  let selecttext = document.getElementById("all").value;
+  localStorage.setItem("dd.value,selecttext") / returntrue;
+}
+document.getElementById("Result").innerHTML = localStorage.setItem("dd.value");
+// When the user clicks on the button, scroll to the top of the document
+function opFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
